a6635dab74e888887cc42e39cef7f1c42a913afd
Sheethal JR <sjr@softwareag.com>
2023-01-10 10:39:29 +0530

    Changes added for DTM - Assets Support

--- src/kpitrend-widget/kpitrend-widget.module.ts
+++ src/kpitrend-widget/kpitrend-widget.module.ts
@@ -27,7 +27,17 @@ import { DatePipe } from '@angular/common';
       description: 'This widget shows the latest measurement value and unit received from a device as a KPI. It compares this measurement value with the average of measurements received in the selected interval and calculated the percentage growth or fall. It allows to configure threshold values to change the KPI color when threshold values are reached. It also shows a trend chart by plotting all the measurement values received for the selected interval or measurements count.',
       component: KPITrendWidget,
       configComponent: KPITrendWidgetConfig,
-      previewImage: require("~assets/img-preview.png")
+      previewImage: require("~assets/img-preview.png"),
+      data: {
+        ng1: {
+          options: {
+            noDeviceTarget: false,
+            noNewWidgets: false,
+            deviceTargetNotRequired: false,
+            groupsSelectable: true
+          }
+        }
+      }
     }
   }, DatePipe],
 })
